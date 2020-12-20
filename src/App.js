316f2f875b5e181e98d919b4f6a8ab6cde40bec8
rahul harlalka <rahulharlalka96@gmail.com>
2020-12-20 19:05:30 +0530

    navbar component added

--- src/App.js
+++ src/App.js
@@ -4,19 +4,23 @@ import Customers from "./components/Customers.jsx";
 import Rentals from "./components/Rentals";
 import NotFound from "./components/NotFound.jsx";
 import { Route, Redirect, Switch } from "react-router-dom";
+import NavBar from "./components/NavBar.jsx";
 
 function App() {
   return (
-    <main className="container">
-      <Switch>
-        <Route path="/movies" component={Movies}></Route>
-        <Route path="/customers" component={Customers}></Route>
-        <Route path="/rentals" component={Rentals}></Route>
-        <Route path="/not-found" component={NotFound}></Route>
-        <Redirect from="/" exact to="/movies" />
-        <Redirect to="/not-found" />
-      </Switch>
-    </main>
+    <React.Fragment>
+      <NavBar />
+      <main className="container">
+        <Switch>
+          <Route path="/movies" component={Movies}></Route>
+          <Route path="/customers" component={Customers}></Route>
+          <Route path="/rentals" component={Rentals}></Route>
+          <Route path="/not-found" component={NotFound}></Route>
+          <Redirect from="/" exact to="/movies" />
+          <Redirect to="/not-found" />
+        </Switch>
+      </main>
+    </React.Fragment>
   );
 }
 
