03bbba2bf6e479c443a782f9e823836234b5e929
rahul harlalka <rahulharlalka96@gmail.com>
2020-12-24 14:48:46 +0530

    JOI vlaidation impementation complete

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,30 +1,30 @@
 import React, { useState } from "react";
 import Input from "./common/Input.jsx";
+import Joi from "joi-browser";
 
 function LoginForm() {
   const [account, setAccount] = useState({ username: "", password: "" });
   const [errors, setErrors] = useState({});
 
+  const schema = {
+    username: Joi.string().required().label("Username"),
+    password: Joi.string().required().label("Password"),
+  };
+
   function validate() {
+    const result = Joi.validate(account, schema, { abortEarly: false });
+    if (!result.error) return null;
     const newErrors = {};
-
-    if (account.username.trim() === "")
-      newErrors.username = "username is required";
-
-    if (account.password.trim() === "")
-      newErrors.password = "password is required";
-
-    return Object.keys(newErrors).length === 0 ? null : newErrors;
+    for (let item of result.error.details)
+      newErrors[item.path[0]] = item.message;
+    return newErrors;
   }
 
-  function validateProperty(input) {
-    if (input.name === "username") {
-      if (input.value.trim() === "") return "username is required";
-    }
-
-    if (input.name === "password") {
-      if (input.value.trim() === "") return "password is required";
-    }
+  function validateProperty({ name, value }) {
+    const obj = { [name]: value };
+    const newSchema = { [name]: schema[name] };
+    const { error } = Joi.validate(obj, newSchema);
+    return error ? error.details[0].message : null;
   }
 
   function handleSubmit(e) {
